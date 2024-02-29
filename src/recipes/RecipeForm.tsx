a41c5bb22c7702ad36740a7cf98ddf5883f4f257
LauritsSchou <laurits.schou@gmail.com>
2024-02-29 17:29:59 +0100

    submit, edit and delete implemented

--- src/recipes/RecipeForm.tsx
+++ src/recipes/RecipeForm.tsx
@@ -1,6 +1,11 @@
 import "./RecipeForm.css";
-import { useState } from "react";
-import { getCategories, addRecipe, deleteRecipe, Recipe } from "../services/apiFacade";
+import { useState, useEffect } from "react";
+import {
+  getCategories,
+  addRecipe,
+  deleteRecipe,
+  Recipe,
+} from "../services/apiFacade";
 import { useLocation } from "react-router-dom";
 
 const EMPTY_RECIPE = {
@@ -16,14 +21,15 @@ const EMPTY_RECIPE = {
 
 export default function RecipeForm() {
   const [categories, setCategories] = useState([""]);
-  //const recipeToEdit = useLocation().state || null;
-  const recipeToEdit = null;
+  const recipeToEdit = useLocation().state || null;
   //const [formData, setFormData] = useState<Recipe>(recipeToEdit || EMPTY_RECIPE);
-  const [formData, setFormData] = useState<Recipe>(recipeToEdit || EMPTY_RECIPE);
+  const [formData, setFormData] = useState<Recipe>(
+    recipeToEdit || EMPTY_RECIPE
+  );
 
-  // useEffect(() => {
-  //   getCategories().then((res) => setCategories(res));
-  // }, []);
+  useEffect(() => {
+    getCategories().then((res) => setCategories(res));
+  }, []);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -33,18 +39,20 @@ export default function RecipeForm() {
     }));
   };
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // e.preventDefault();
-    // if (formData.id) {
-    //   deleteRecipe(Number(formData.id));
-    //   setFormData({ ...EMPTY_RECIPE });
-    // }
+    e.preventDefault();
+    if (formData.id) {
+      deleteRecipe(Number(formData.id));
+      setFormData({ ...EMPTY_RECIPE });
+    }
   };
 
   const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    // e.preventDefault();
-    // const newRecipe = await addRecipe(formData);
-    // alert("New recipe added")
-    // console.info("New/Edited Recipe", newRecipe);
+    e.preventDefault();
+    const addedOrEdited = formData.id ? "edited" : "added";
+    const newRecipe = await addRecipe(formData);
+    alert(`Recipe ${addedOrEdited} successfully!`);
+    setFormData({ ...EMPTY_RECIPE });
+    console.log("newRecipe", newRecipe);
   };
 
   return (
@@ -53,11 +61,24 @@ export default function RecipeForm() {
       <form id="recipeForm">
         <div className="form-group">
           <label htmlFor="id">ID:</label>
-          <input type="text" id="id" name="id" disabled value={formData.id || ""} />
+          <input
+            type="text"
+            id="id"
+            name="id"
+            disabled
+            value={formData.id || ""}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="category">Category:</label>
@@ -92,11 +113,23 @@ export default function RecipeForm() {
         </div>
         <div className="form-group">
           <label htmlFor="thumb">Thumbnail URL:</label>
-          <input type="text" id="thumb" name="thumb" value={formData.thumb} onChange={handleChange} />
+          <input
+            type="text"
+            id="thumb"
+            name="thumb"
+            value={formData.thumb}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="youTube">YouTube URL:</label>
-          <input type="text" id="youTube" name="youTube" value={formData.youTube} onChange={handleChange} />
+          <input
+            type="text"
+            id="youTube"
+            name="youTube"
+            value={formData.youTube}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="ingredients">Ingredients:</label>
@@ -112,10 +145,17 @@ export default function RecipeForm() {
         </div>
         <div className="form-group">
           <label htmlFor="source">Source:</label>
-          <input type="text" id="source" name="source" value={formData.source} onChange={handleChange} required />
+          <input
+            type="text"
+            id="source"
+            name="source"
+            value={formData.source}
+            onChange={handleChange}
+            required
+          />
         </div>
       </form>
-      <button type="submit" className="recipe-form-btn">
+      <button type="submit" className="recipe-form-btn" onClick={handleSubmit}>
         Submit
       </button>
       <button
